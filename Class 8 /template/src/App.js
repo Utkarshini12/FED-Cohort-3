ae571b291192ced9af0d215bcd8053d8d364e904
Utkarshini12 <50311757+Utkarshini12@users.noreply.github.com>
2022-03-28 23:02:51 +0530

    Add files via upload

---
+++ Class 8 /template/src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return(
+    <div>
+   
+    
+    <Footer />
+   
+    <div>
+      <h1>Hello , React!</h1>
+    </div>
+    <Navbar />
+    </div>
+  )
+
+
+}
+ 
+
+export default App
